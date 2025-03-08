6cdaab5d6750e93b4a4e63f9ac593b3d91aece68
unnamedhn <unnamed.h@gmail.com>
2025-03-08 17:18:14 +0330

    enable decompression fallback with brotli

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "echenDeligani-portfolio-0.1.0";
 const contentToCache = [
     "Build/Echen.loader.js",
-    "Build/Echen.framework.js",
-    "Build/Echen.data",
-    "Build/Echen.wasm",
+    "Build/Echen.framework.js.unityweb",
+    "Build/Echen.data.unityweb",
+    "Build/Echen.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
